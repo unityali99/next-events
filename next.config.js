1d6ab564e2ed1440ebc32eda0936544e90694914
Ali <mrkiller1383@gmail.com>
2023-02-14 20:24:02 +0330

    Fixed db name which caused the bug

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     apiUrl: "http://localhost:3000/api",
-    dbName: "Next-Events",
+    dbName: "events",
     uri: "mongodb://localhost:27017",
   },
   reactStrictMode: true,
